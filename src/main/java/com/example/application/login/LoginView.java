fc1d676b68fb8b857ba67601a8df8c38ec041dc3
SharaevOleg/Tvr <tverskoi2@mail.ru>
2021-06-14 03:48:48 +0300

    rewroteIt

--- src/main/java/com/example/application/login/LoginView.java
+++ src/main/java/com/example/application/login/LoginView.java
@@ -1,14 +1,26 @@
 package com.example.application.login;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
+import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.login.LoginI18n;
 import com.vaadin.flow.component.login.LoginOverlay;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
+
+import javax.swing.*;
 
 @Route(value = "hello")
 @RouteAlias(value = "")
@@ -17,41 +29,54 @@ public class LoginView extends HorizontalLayout {
 
 
     public LoginView() {
-        LoginOverlay component = new LoginOverlay();
-        H1 title = new H1();
-        title.getStyle().set("color", "var(--lumo-base-color)");
-        title.add(new Text("MEDIUM 2.0"));
-        component.setTitle(title);
 
-        component.addLoginListener(e -> component.close());
+//        Форма диалогового окна и кнопка перехода на форму
 
-        LoginI18n i18n = LoginI18n.createDefault();
-        i18n.setAdditionalInformation("Чтобы закрыть форму входа, заполните имя пользователя и пароль");
-        i18n.setHeader(new LoginI18n.Header());
-        i18n.getHeader().setDescription("платформа для социальной журналистики");
-        i18n.getForm().setUsername("Пользователь (E-mail)");
-        i18n.getForm().setTitle("Войдите в свою учетную запись");
-        i18n.getForm().setPassword("Пароль");
-        i18n.getForm().setForgotPassword("Я забыл свой пароль");
-        i18n.getErrorMessage().setTitle("Пользователь/пароль неверные");
-        i18n.getErrorMessage().setMessage("Проверьте свое имя пользователя и пароль и попробуйте еще раз.");
+        ConfirmDialog dialog = new ConfirmDialog("MEDIUM 2.0",
+                "Чтобы закрыть форму входа, заполните имя пользователя и пароль", "Log in", this::onPublish,
+                "Exit", this::onCancel);
+        Button button = new Button("Вход");
+        button.addClickListener(event -> dialog.open());
+        add(button, dialog);
 
+//        Добавляем поля
+        VerticalLayout layout = new VerticalLayout();
+        layout.setJustifyContentMode(JustifyContentMode.CENTER);
 
-        component.setI18n(i18n);
+        layout.setDefaultHorizontalComponentAlignment(
+                FlexComponent.Alignment.STRETCH);
 
+        Text text = new Text("Войдите в свою учетную запись");
 
-        Button open = new Button(" ВХОД ",
-                e -> component.setOpened(true));
+        EmailField emailField = new EmailField("Пользователь (E-mail)");
+        emailField.setClearButtonVisible(true);
+        emailField.setErrorMessage("Пожалуйста, введите действительный адрес электронной почты");
 
-        add(component, open);
+        PasswordField passwordField = new PasswordField();
+        passwordField.setLabel("Пароль");
+        passwordField.setPlaceholder("Введите пароль");
+        passwordField.setValue("пароль!");
 
-//        Checkbox checkbox = new Checkbox();
-//        checkbox.setLabel("Запомни меня");
-//        checkbox.setValue(false);
-//        add(checkbox);
+        Checkbox checkbox = new Checkbox();
+        checkbox.setLabel("запомните меня");
+        checkbox.setValue(false);
 
+        Anchor anchor = new Anchor("https://vaadin.com", "Я забыл свой пароль");
+        anchor.getElement().addEventListener("click", e -> Notification.show("переходим в новое окно"));
 
+        layout.add(text, emailField, passwordField, checkbox, anchor);
+
+        dialog.add(layout);
     }
+
+
+//    Методы на кнопки Входа/авторизации и выхода из формы
+    private void onPublish(ConfirmDialog.ConfirmEvent confirmEvent) {
+    }
+
+    private void onCancel(ConfirmDialog.CancelEvent cancelEvent) {
+    }
+
 
 }
 
