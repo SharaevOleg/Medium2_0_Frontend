9ecf5e417f52080edc9e98d348f77a651a34b065
SharaevOleg/Tvr <tverskoi2@mail.ru>
2021-06-16 03:26:44 +0300

    rewroteIt4

--- src/main/java/com/example/application/login/LoginView.java
+++ src/main/java/com/example/application/login/LoginView.java
@@ -1,75 +1,68 @@
 package com.example.application.login;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
-import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
-import com.vaadin.flow.component.html.Anchor;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouteAlias;
 
-@Route(value = "hello")
-@RouteAlias(value = "")
+@Route(value = "/login")
 @PageTitle("Страница авторизации")
+
 public class LoginView extends HorizontalLayout {
 
+    private final Text text = new Text("Войдите в свою учетную запись");
 
     public LoginView() {
 
-//        Форма диалогового окна и кнопка перехода на форму
+        setWidthFull();
+        setHeightFull();
+        add(loginFormConfig());
+    }
 
-        ConfirmDialog dialog = new ConfirmDialog("MEDIUM 2.0",
-                "Чтобы закрыть форму входа, заполните имя пользователя и пароль", "Log in", this::onPublish,
-                "Exit", this::onCancel);
-        Button button = new Button("Вход");
-        button.addClickListener(event -> dialog.open());
-        add(button, dialog);
+    private Component h1Cofig() {
+        H1 h1Field = new H1("MEDIUM 2.0");
+        return h1Field;
+    }
 
-//        Добавляем поля
-        VerticalLayout layout = new VerticalLayout();
-        layout.setJustifyContentMode(JustifyContentMode.CENTER);
-
-        layout.setDefaultHorizontalComponentAlignment(
-                FlexComponent.Alignment.STRETCH);
-
-        Text text = new Text("Войдите в свою учетную запись");
-
+    private Component emailFieldConfig() {
         EmailField emailField = new EmailField("Пользователь (E-mail)");
         emailField.setClearButtonVisible(true);
         emailField.setErrorMessage("Пожалуйста, введите действительный адрес электронной почты");
+        return emailField;
+    }
 
+    private Component passwordFieldConfig() {
         PasswordField passwordField = new PasswordField();
         passwordField.setLabel("Пароль");
         passwordField.setPlaceholder("Введите пароль");
-        passwordField.setValue("пароль!");
+        return passwordField;
+    }
 
+    private Component checkboxConfig() {
         Checkbox checkbox = new Checkbox();
-        checkbox.setLabel("запомните меня");
+        checkbox.setLabel("запомнить меня");
         checkbox.setValue(false);
-
-        Anchor anchor = new Anchor("https://vaadin.com", "Я забыл свой пароль");
-        anchor.getElement().addEventListener("click", e -> Notification.show("переходим в новое окно"));
-
-        layout.add(text, emailField, passwordField, checkbox, anchor);
-
-        dialog.add(layout);
+        return checkbox;
     }
 
-
-//    Методы на кнопки Входа/авторизации и выхода из формы
-    private void onPublish(ConfirmDialog.ConfirmEvent confirmEvent) {
+    private Component buttonConfig() {
+        Button button = new Button("Войти");
+        return button;
     }
 
-    private void onCancel(ConfirmDialog.CancelEvent cancelEvent) {
+    private Component loginFormConfig() {
+        VerticalLayout loginFormVerticalLayout = new VerticalLayout();
+        loginFormVerticalLayout.setJustifyContentMode(JustifyContentMode.CENTER);
+        loginFormVerticalLayout.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        loginFormVerticalLayout.add(h1Cofig(), text, emailFieldConfig(), passwordFieldConfig(), checkboxConfig(), buttonConfig());
+        return loginFormVerticalLayout;
     }
-
-
 }
 
